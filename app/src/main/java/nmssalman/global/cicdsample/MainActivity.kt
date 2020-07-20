8a9c8a2f9a56e979e38b34d77ddd758b365ccaf4
salman <nmssalman@outlook.com>
2020-07-21 03:00:25 +0900

    fixes

--- app/src/main/java/nmssalman/global/cicdsample/MainActivity.kt
+++ app/src/main/java/nmssalman/global/cicdsample/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textVersion.setText(BuildConfig.VERSION_NAME)
+        textVersion.setText("Version Name: " + BuildConfig.VERSION_NAME)
     }
 }
